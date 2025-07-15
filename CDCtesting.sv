25947f4eed1d19944aa7037f09f785c91b1dd0de
Matthew Smith <43557713+MatthewSmith413@users.noreply.github.com>
2025-07-15 16:49:15 -0400

    Add files via upload
    contains several modules that interact with the testbench. Each module lists what it will do in the test

---
+++ CDCtesting.sv
@@ -0,0 +1,51 @@
+module CDCtesting(
+	input reset, TXData, TXClk, RXClk,
+	output RXData, TXReady, RXReady
+);
+	// use this line to connect a CDC module to the 
+	FIFO cdcmodule(reset, TXData, TXClk, RXClk, RXData, TXReady, RXReady);
+endmodule
+
+module failAll(
+	input reset, TXData, TXClk, RXClk,
+	output RXData, TXReady, RXReady
+);
+	// this module will fail all tests.
+	assign RXData = 1'b0;
+	assign TXReady = 1'b1;
+	assign RXReady = 1'b1;
+endmodule
+
+module stallAll(
+	input reset, TXData, TXClk, RXClk,
+	output RXData, TXReady, RXReady
+);
+	// this module will stall indefinitely.
+	assign RXData = 1'b0;
+	assign TXReady = 1'b0;
+	assign RXReady = 1'b0;
+endmodule
+
+
+module FIFO #(parameter WIDTH = 8)(
+	input reset, TXData, TXClk, RXClk,
+	output RXData, TXReady, RXReady
+);
+	// this module will pass all tests
+	logic [WIDTH-1:0] TXIndex, RXIndex, diff;
+	logic [0:0] ram[1<<WIDTH];
+	always @(posedge TXClk) begin
+		ram[TXIndex] <= TXData;
+	end
+	always @(posedge TXClk or posedge reset) begin
+		TXIndex <= reset?0:TXReady?TXIndex+1:TXIndex;
+	end
+	always @(posedge RXClk or posedge reset) begin
+		RXIndex <= reset?0:RXReady?RXIndex+1:RXIndex; // if reset, turn off. if RXReady, increment.
+	end
+	assign RXData = ram[RXIndex];
+	assign RXReady = RXIndex != TXIndex;
+	assign TXReady = diff!=3;
+	assign diff = RXIndex-TXIndex;
+	
+endmodule
